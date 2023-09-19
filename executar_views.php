0f5422dc0c51678c5d781fb5d69780310e831db0
aaraoeller <89228715+aaraoeller@users.noreply.github.com>
2023-09-19 14:54:47 -0400

    Update executar_views.php

--- executar_views.php
+++ executar_views.php
@@ -33,47 +33,16 @@ if ($result) {
         while ($row = $result->fetch_assoc()) {
             // Exiba os resultados de todas as colunas
             foreach ($row as $value) {
-                echo $value . " ";
+                echo $value;
             }
             echo "<br>";
         }
-    }
-
-if ($viewName === "Lista geral de contas") {
-    echo "<h2>Resultado da View: " . $viewName . "</h2>";
-
-    if ($result && $result->num_rows > 0) {
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>Descrição</th>";
-        echo "<th>Valor a Pagar</th>";
-        echo "<th>Vencimento Pagamento</th>";
-        echo "<th>Descrição</th>";
-        echo "<th>Valor a Receber</th>";
-        echo "<th>Vencimento Recebimento</th>";
-        echo "</tr>";
-
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row['DESCRICAO'] . "</td>";
-            echo "<td>" . $row['VALOR_PAGAR'] . "</td>";
-            echo "<td>" . $row['VENCIMENTO_PAGAMENTO'] . "</td>";
-            echo "<td>" . $row['DESCIRCAO'] . "</td>";
-            echo "<td>" . $row['VALOR_RECEBER'] . "</td>";
-            echo "<td>" . $row['VENCIMENTO_RECEBIMENTO'] . "</td>";
-            echo "</tr>";
-        }
-
-        echo "</table>";
     } else {
         echo "A consulta não retornou resultados.";
     }
-}
 } else {
     echo "Erro ao executar a view: " . $mysqli->error;
 }
 
 $mysqli->close();
 ?>
-
-<?php
