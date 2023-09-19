0911a318ca9f90c0600efb9a3e82af29f02a88f6
aaraoeller <89228715+aaraoeller@users.noreply.github.com>
2023-09-19 13:26:08 -0400

    .

---
+++ executar_views.php
@@ -0,0 +1,28 @@
+<?php
+// Conexão com o banco de dados
+$mysqli = new mysqli("localhost", "root@localhost", "", "contas");
+
+if ($mysqli->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $mysqli->connect_error);
+}
+
+// Verificar qual botão foi clicado
+if (isset($_POST['total_pagar'])) {
+    $query = "SELECT * FROM V_TOTAL_PAGAR";
+} elseif (isset($_POST['total_receber'])) {
+    $query = "SELECT * FROM V_TOTAL_RECEBER";
+} elseif (isset($_POST['total_geral'])) {
+    $query = "SELECT * FROM V_CONTAS_TOTAL";
+}
+
+$result = $mysqli->query($query);
+
+if ($result) {
+    $row = $result->fetch_assoc();
+    echo "Resultado: " . $row['TOTAL_A_PAGAR'];
+} else {
+    echo "Erro ao executar a view: " . $mysqli->error;
+}
+
+$mysqli->close();
+?>
---
+++ inserir_conta.php
@@ -0,0 +1,29 @@
+<?php
+// Conexão com o banco de dados
+$mysqli = new mysqli("localhost", "root@localhost", "", "contas");
+
+if ($mysqli->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $mysqli->connect_error);
+}
+
+// Recuperar dados do formulário
+$descricao = $_POST['descricao'];
+$valor = $_POST['valor'];
+$data = $_POST['data'];
+$tipo = $_POST['tipo'];
+
+// Inserir dados de acordo com o tipo (pagar ou receber)
+if ($tipo === 'pagar') {
+    $query = "CALL P_INS_CONTAS_PAGAR('$descricao', $valor, '$data')";
+} else {
+    $query = "CALL P_INS_CONTAS_RECEBER('$descricao', $valor, '$data')";
+}
+
+if ($mysqli->query($query)) {
+    echo "Conta inserida com sucesso!";
+} else {
+    echo "Erro ao inserir conta: " . $mysqli->error;
+}
+
+$mysqli->close();
+?>
