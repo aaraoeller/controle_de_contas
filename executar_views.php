8a511dab3e4d8370da413fc13543dbfd36e02830
aaraoeller <89228715+aaraoeller@users.noreply.github.com>
2023-09-19 14:19:32 -0400

    .

--- executar_views.php
+++ executar_views.php
@@ -9,20 +9,28 @@ if ($mysqli->connect_error) {
 // Verificar qual botão foi clicado
 if (isset($_POST['total_pagar'])) {
     $query = "SELECT * FROM V_TOTAL_PAGAR";
+    $viewName = "Total a Pagar";
 } elseif (isset($_POST['total_receber'])) {
     $query = "SELECT * FROM V_TOTAL_RECEBER";
+    $viewName = "Total a Receber";
 } elseif (isset($_POST['total_geral'])) {
     $query = "SELECT * FROM V_CONTAS_TOTAL";
+    $viewName = "Total Geral";
 }
 
 $result = $mysqli->query($query);
 
 if ($result) {
-    $row = $result->fetch_assoc();
-    echo "Resultado: " . $row['total_pagar'];
-}
-
-else {
+    // Exibe o nome da view
+    echo "Resultado da View: " . $viewName . "<br>";
+    
+    // Processa os resultados da consulta
+    while ($row = $result->fetch_assoc()) {
+        // Aqui você pode acessar os resultados da consulta específica
+        // Exemplo: echo "Descrição: " . $row['DESCRICAO'] . "<br>";
+        // Lembre-se de usar os nomes das colunas das views corretamente
+    }
+} else {
     echo "Erro ao executar a view: " . $mysqli->error;
 }
 
